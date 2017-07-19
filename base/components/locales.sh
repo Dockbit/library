ba1ae3f452d7d78d661632ae2574be38edcc92ff
Tair Assimov <tair@dockbit.com>
2017-07-19 17:33:58 +0200

    Remove extra space from locales exports

--- base/components/locales.sh
+++ base/components/locales.sh
@@ -8,7 +8,7 @@ echo "en_US UTF-8" > /etc/locale.gen
 locale-gen en_US.UTF-8
 echo 'export LANG=en_US.UTF-8' >> /home/dockbit/.bash_profile
 echo 'export LANGUAGE=en_US:en' >> /home/dockbit/.bash_profile
-echo 'export  LC_ALL=en_US.UTF-8' >> /home/dockbit/.bash_profile
+echo 'export LC_ALL=en_US.UTF-8' >> /home/dockbit/.bash_profile
 
 # Cleanup
 rm -rf /var/lib/apt/lists/*
